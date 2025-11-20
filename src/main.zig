9da81305fe1e951157eeceebeac81b9c77dafa50
Nathaniel Ketema <nathanielketema@gmail.com>
2025-11-19 19:21:14 -0600

    feat: start utilizing the new files

--- src/main.zig
+++ src/main.zig
@@ -2,29 +2,28 @@ const std = @import("std");
 const assert = std.debug.assert;
 const Io = std.Io;
 
+const vec3 = @import("vector.zig");
+const color = @import("color.zig");
+
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-
     const image_width = 256;
     const image_height = 256;
 
-    try stdout.print("P3\n{d} {d}\n255\n", .{image_width, image_height});
+    try stdout.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
     for (0..image_height) |j| {
         std.log.info("\rScanlines remaining: {d} ", .{image_height - j});
         for (0..image_width) |i| {
-            const r: f64 = @as(f64, @floatFromInt(i)) / (image_width - 1);
-            const g: f64 = @as(f64, @floatFromInt(j)) / (image_height - 1);
-            const b: f64 = 0.0;
-
-            const ir: i32 = @intFromFloat(255.999 * r);
-            const ig: i32 = @intFromFloat(255.999 * g);
-            const ib: i32 = @intFromFloat(255.999 * b);
-
-            try stdout.print("{d} {d} {d}\n", .{ir, ig, ib});
+            const pixel_color = color.Color.init(
+                @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(image_width - 1)),
+                @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(image_height - 1)),
+                0.0,
+            );
+            try color.write(stdout, pixel_color);
         }
     }
     std.log.info("\rDone.                 \n", .{});
